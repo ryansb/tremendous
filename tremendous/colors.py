c4ff12de27e860aea8b9e13a8e67df06e93197fe
Ryan S. Brown <sb@ryansb.com>
2014-10-31 09:00:28 -0400

    declare colors as top-level

---
+++ tremendous/colors.py
@@ -0,0 +1,34 @@
+from tremendous.bindings import lib as __lib
+
+
+BOLD = __lib.BOLD
+ITALIC = __lib.ITALIC
+UNDER = __lib.UNDER
+UNDER2 = __lib.UNDER2
+STRIKE = __lib.STRIKE
+BLINK = __lib.BLINK
+FLIP = __lib.FLIP
+BLACK = __lib.BLACK
+RED = __lib.RED
+GREEN = __lib.GREEN
+YELLOW = __lib.YELLOW
+BLUE = __lib.BLUE
+MAGENTA = __lib.MAGENTA
+CYAN = __lib.CYAN
+WHITE = __lib.WHITE
+HBLACK = __lib.HBLACK
+HRED = __lib.HRED
+HGREEN = __lib.HGREEN
+HYELLOW = __lib.HYELLOW
+HBLUE = __lib.HBLUE
+HMAGENTA = __lib.HMAGENTA
+HCYAN = __lib.HCYAN
+HWHITE = __lib.HWHITE
+BGBLACK = __lib.BGBLACK
+BGRED = __lib.BGRED
+BGGREEN = __lib.BGGREEN
+BGYELLOW = __lib.BGYELLOW
+BGBLUE = __lib.BGBLUE
+BGMAGENTA = __lib.BGMAGENTA
+BGCYAN = __lib.BGCYAN
+BGWHITE = __lib.BGWHITE
