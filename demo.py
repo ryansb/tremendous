69b275a4e42daa71e7b59caf55cf73f501c404c5
Ryan S. Brown <sb@ryansb.com>
2014-10-31 13:45:57 -0400

    add demo script

---
+++ demo.py
@@ -0,0 +1,8 @@
+from tremendous import red, green, underline, underline2, blink, bold, italic, bg_red
+print green("fizz")
+print red("fizz")
+print bg_red(green("fizz"))
+print bold(red("fizz"))
+print underline(green("fizz"))
+print underline2(red("fizz"))
+print blink(green("fizz"))
