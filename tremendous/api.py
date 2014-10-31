635d0db275fdf9ceba252432944d06949556a8f8
Ryan S. Brown <sb@ryansb.com>
2014-10-31 13:45:11 -0400

    remove apply_formats function

--- tremendous/api.py
+++ tremendous/api.py
@@ -4,12 +4,3 @@ from tremendous.bindings import ffi
 def apply_format(color, body):
     s = lib.apply_format(color, body)
     return ffi.string(s)
-
-def apply_formats(colors, body):
-    if isinstance(colors, int):
-        colors = [colors]
-
-    for c in colors:
-        body = apply_format(c, body)
-
-    return body
