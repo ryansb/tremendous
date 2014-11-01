920072b1495a1adfa5ab2b31078514325d1cccbf
Ryan S. Brown <sb@ryansb.com>
2014-10-31 20:39:38 -0400

    add 256 color functions

--- tremendous/api.py
+++ tremendous/api.py
@@ -1,6 +1,22 @@
 from tremendous.bindings import lib
 from tremendous.bindings import ffi
 
+
 def apply_format(color, body):
     s = lib.apply_format(color, body)
+    return ffi.string(s)
+
+
+def apply_256(rgb_t, body):
+    s = lib.foreground_256(rgb_t, body)
+    return ffi.string(s)
+
+
+def apply_256_bg(rgb_t, body):
+    s = lib.background_256(rgb_t, body)
+    return ffi.string(s)
+
+
+def apply_256_hl(rgb_t, body):
+    s = lib.highlight_256(rgb_t, body)
     return ffi.string(s)
