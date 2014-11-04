1076a7ae9a8c9cd0ea0ad1d99d981acc89270e25
Ryan S. Brown <sb@ryansb.com>
2014-11-03 20:50:16 -0500

    Python3 support

--- tremendous/api.py
+++ tremendous/api.py
@@ -3,20 +3,36 @@ from tremendous.bindings import ffi
 
 
 def apply_format(color, body):
-    s = lib.apply_format(color, body)
-    return ffi.string(s)
+    try:
+        s = lib.apply_format(color, body)
+        return ffi.string(s)
+    except:
+        s = lib.apply_format(color, bytes(body, 'utf-8'))
+        return ffi.string(s).decode('utf-8')
 
 
 def apply_256(rgb_t, body):
-    s = lib.foreground_256(rgb_t, body)
-    return ffi.string(s)
+    try:
+        s = lib.foreground_256(rgb_t, body)
+        return ffi.string(s)
+    except:
+        s = lib.foreground_256(rgb_t, bytes(body, 'utf-8'))
+        return ffi.string(s).decode('utf-8')
 
 
 def apply_256_bg(rgb_t, body):
-    s = lib.background_256(rgb_t, body)
-    return ffi.string(s)
+    try:
+        s = lib.background_256(rgb_t, body)
+        return ffi.string(s)
+    except:
+        s = lib.background_256(rgb_t, bytes(body, 'utf-8'))
+        return ffi.string(s).decode('utf-8')
 
 
 def apply_256_hl(rgb_t, body):
-    s = lib.highlight_256(rgb_t, body)
-    return ffi.string(s)
+    try:
+        s = lib.highlight_256(rgb_t, body)
+        return ffi.string(s)
+    except:
+        s = lib.highlight_256(rgb_t, bytes(body, 'utf-8'))
+        return ffi.string(s).decode('utf-8')
